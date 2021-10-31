11308e7193acc87b4e6c2a7bc86dd657a605b3fb
Artem Pavlov <pavlov.tema@gmail.com>
2021-10-31 22:30:30 +0500

    Initial Commit

---
+++ ColorMix2/ColorMix2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMix2App.swift
+//  ColorMix2
+//
+//  Created by iMac on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorMix2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
