7c8959a0dbd2b41febfe116031a8743fd68bc750
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-01 00:07:40 +0500

    SliderView added.

---
+++ ColorMix2/SliderView.swift
@@ -0,0 +1,40 @@
+//
+//  Slider.swift
+//  ColorMix2
+//
+//  Created by Artem Pavlov on 31.10.2021.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    @State private var sliderValue = Double.random(in: 0...255)
+    
+    let color: Color
+    
+    var body: some View {
+        SliderParametrs(value: $sliderValue, color: color)
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView(color: .green)
+    }
+}
+
+struct SliderParametrs: View {
+    @Binding var value: Double
+    
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(color)
+            Text("\(lround(value))")
+                .frame(width: 35)
+        }
+    }
+}
