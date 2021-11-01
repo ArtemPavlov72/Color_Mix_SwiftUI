a26b53d26d2b1d631f8327aebb689f6c296b6a60
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-01 15:38:01 +0500

    ColorMixView added.

---
+++ ColorMix2/ColorMixView.swift
@@ -0,0 +1,37 @@
+//
+//  ColorMixView.swift
+//  ColorMix2
+//
+//  Created by Artem Pavlov on 01.11.2021.
+//
+
+import SwiftUI
+
+struct ColorMixView: View {
+    let red: Double
+    let green: Double
+    let blue: Double
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 40)
+            .foregroundColor(
+                Color(
+                    red: red/255,
+                    green: green/255,
+                    blue: blue/255
+                )
+            )
+            .frame(width: 350, height: 150)
+            .overlay(
+                RoundedRectangle(cornerRadius: 40)
+                    .stroke(Color.white, lineWidth: 10)
+            )
+    }
+}
+
+struct ColorMixView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorMixView(red: 40, green: 50, blue: 40)
+    }
+}
+
